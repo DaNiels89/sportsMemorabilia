447c1a078c1060db695bd53caa277a21a76bb52c
DaNiels89 <nielssloesen@hotmail.com>
2020-02-22 12:43:31 -0500

    Added active storage

--- .history/app/models/user_20200222113250.rb
+++ .history/app/models/user_20200222113250.rb
@@ -1,9 +1,6 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :pic
 end
